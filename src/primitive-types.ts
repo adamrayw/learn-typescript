6f148813727a3bced808b909ed21f28943d23a23
adamrayw <raywibowo68@gmail.com>
2022-12-31 08:16:32 +0700

    Primitive Types

---
+++ src/primitive-types.ts
@@ -0,0 +1,8 @@
+// There are 7 primitive in JS:
+// string, number, bigInt, boolean, undefined, null and symbol
+
+// Explicit type annotation
+let fistName: string = 'Adam'
+
+// if we assign a value (as above), we don't need to state the type - TS will infer it ("Implicit type annotation")
+let firstName = 'Adam'
