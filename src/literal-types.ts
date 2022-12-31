ea9419a198f340ea22f9db8a8923fd9d84c35aa0
adamrayw <raywibowo68@gmail.com>
2022-12-31 08:23:51 +0700

    Literal Types

---
+++ src/literal-types.ts
@@ -0,0 +1,6 @@
+// we can refer to specific strings & numbers in type positions
+let direction: 'UP' | 'DOWN'
+
+direction = 'LEFT'; // Error: Type '"LEFT"' is not assignable to type '"UP" | "DOWN"'.
+
+direction = 'UP'; // Not Error
