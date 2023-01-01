fac44450078b5577d890f1ed1272b58a8952c287
adamrayw <raywibowo68@gmail.com>
2023-01-01 14:40:33 +0700

    Objects

---
+++ src/objects.ts
@@ -0,0 +1,14 @@
+// objects in TS must have all the correct properties & value types
+
+let person: {
+    name: string;
+    isProgrammer: boolean;
+}
+
+person = {
+    name: 'Adam',
+    isProgrammer: true
+}
+
+person.name = 23 // Error: Type 'number' is not assignable to type 'string'.
+person.isProgrammer = 'false' // Error: Type 'string' is not assignable to type 'boolean'.
