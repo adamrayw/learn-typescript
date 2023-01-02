75bc77cb0a6cf2b7fb4b8620d4c485cb615b7cff
adamrayw <raywibowo68@gmail.com>
2023-01-02 22:22:22 +0700

    Tuples

---
+++ src/tuples.ts
@@ -0,0 +1,8 @@
+// A tuple is a special type of array with fixed size & known 
+// data types at each index. they're stricter
+
+let infoPribadi: [string, number];
+
+infoPribadi = ['Adam', 20] // OKAY
+
+infoPribadi = ['Adam', '20'] // Error: Type 'string' is not assignable to type 'number'.
