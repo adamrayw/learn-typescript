5a148b825d8758132b6ed9aa7448bb161065cc25
adamrayw <raywibowo68@gmail.com>
2023-01-03 20:01:49 +0700

    add comment

--- src/objects.ts
+++ src/objects.ts
@@ -10,5 +10,5 @@ person = {
     isProgrammer: true
 }
 
-person.name = 23 // Error: Type 'number' is not assignable to type 'string'.
-person.isProgrammer = 'false' // Error: Type 'string' is not assignable to type 'boolean'.
+// person.name = 23 // Error: Type 'number' is not assignable to type 'string'.
+// person.isProgrammer = 'false' // Error: Type 'string' is not assignable to type 'boolean'.
